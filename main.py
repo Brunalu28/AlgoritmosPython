f5ab0a1613b5967d9d48427c61c4b67f6cc19842
Luiza Bruna <bluiza747@gmail.com>
2022-02-15 13:32:06 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+## Calculadora em Python ##
+n1 = int(input("Digite o primeiro número: "))
+n2 = int(input("Digite o segundo número: "))
+
+S = n1 + n2 
+D = n1 / n2
+M = n1*n2
+SB = n1 - n2
+
+print("Adicione a operação que você deseja realizar:")
+print("Soma (S)")
+print("Divisão (D)")
+print("Multplicação (M)")
+print("Subtração (SB)")
+
+Menu = S, SB, D, M
+
+if Menu == S:
+  print(S)
+
+if Menu == D:
+  print(D)
+
+if Menu == SB:
+  print(SB)
+
+if Menu == M:
+  print(M)
