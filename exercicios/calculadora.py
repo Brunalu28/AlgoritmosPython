6de48f51dc5c1e644e75e22b69b67e7f5d309f1d
Your Name <youremail@domain.com>
2022-08-27 21:20:34 -0300

    Calculadora funcionando

--- exercicios/calculadora.py
+++ exercicios/calculadora.py
@@ -5,26 +5,26 @@ print("CALCULADORA EM PYTHON")
 n1 = int(input("Digite o primeiro número: "))
 n2 = int(input("Digite o segundo número: "))
 
-S = n1 + n2 
-D = n1 / n2
-M = n1*n2
-SB = n1 - n2
-
 print("Soma (S)")
 print("Divisão (D)")
 print("Multplicação (M)")
 print("Subtração (SB)")
 
-Menu = S, SB, D, M
+operacao = input("Adicione a operação que você deseja realizar: ")
 
-if Menu == S:
+S = (n1 + n2)
+D = (n1 / n2)
+M = (n1*n2)
+SB = (n1 - n2)
+
+if operacao == "S":
   print(S)
 
-if Menu == D:
+if operacao == "D":
   print(D)
 
-if Menu == SB:
+if operacao == "SB":
   print(SB)
 
-if Menu == M:
+if operacao == "M":
   print(M)
