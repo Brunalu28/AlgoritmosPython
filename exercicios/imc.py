e7b387a8ed280d8c5e5d234dbf77ad292b5c09f9
Your Name <youremail@domain.com>
2022-09-11 21:31:09 -0300

    Novos exercícios

---
+++ exercicios/imc.py
@@ -0,0 +1,6 @@
+massa = float(input())
+alt = float(input())
+
+imc = (massa/alt**2)
+
+print ("%.2f" % imc)
---
+++ exercicios/retangulo.py
@@ -0,0 +1,8 @@
+raio = float(input())
+angulo = float(input())
+
+comp = raio*angulo
+
+area = 3.14*(raio**2)
+
+print(comp , area)
